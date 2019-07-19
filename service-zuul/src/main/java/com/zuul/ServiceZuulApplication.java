fa7f94f1da93d3d27144f12cf8710314afe49d6d
xuezk <xzk@ihotel.cn>
2019-07-19 16:34:15 +0800

    ZUUL 网关的引入

---
+++ service-zuul/src/main/java/com/zuul/ServiceZuulApplication.java
@@ -0,0 +1,18 @@
+package com.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author xzk
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ServiceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceZuulApplication.class, args);
+	}
+
+}
