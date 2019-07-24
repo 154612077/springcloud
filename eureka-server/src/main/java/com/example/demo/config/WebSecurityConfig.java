0909255b82d78fd855ac81852ec5725a49519666
xuezk <xzk@ihotel.cn>
2019-07-24 09:57:27 +0800

    eureka 验证

---
+++ eureka-server/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author xzk
+ * @date 2019/7/24
+ **/
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();//关闭csrf
+        super.configure(http);
+    }
+
+}
